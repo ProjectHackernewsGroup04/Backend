de4b07485cc9e0ef56a297f6c22570dcfc0df5a8
Andreas Styltsvig <ansty93@gmail.com>
2018-09-27 16:16:28 +0200

    Adding controller.py as logical layer

--- app/app.py
+++ app/app.py
@@ -1,18 +1,8 @@
 from flask import Flask, url_for, request, session, redirect, jsonify
 from bson.json_util import dumps
-import bcrypt
-
-# Database imports
-from database import (
-    collection_name_items,
-    get_db_conn,
-    prepare_db,
-)
+import controller
 
 app = Flask(__name__)
-
-# Global variables
-db_con = get_db_conn()
 app.config['MONGO_DBNAME'] = 'hackernews'
 
 
@@ -22,48 +12,37 @@ def api_home():
         return 'You are logged in as ' + session['username']
     return redirect(url_for('/'))
 
-
 # Login
 @app.route('/api/login', methods=['POST'])
 def api_login():
-    users = db_con.users
     content = request.json
     username = content['username']
     password = content['password']
-    login_user = users.find_one({'username': username})
-    app.logger.info('Trying loggin')
-    app.logger.info(username)
-    if login_user:
-        stored_password = login_user['password']
-        ## Comparing stored password and the users hashed password
-        if bcrypt.hashpw(password.encode('utf8'), stored_password) == stored_password: 
-            app.logger.info('Login Success')
-            return jsonify({'statusCode': 200,
+    app.logger.info('Trying Login')
+    if controller.checkIfloginSuccess(username,password):
+        app.logger.info('Login Success')
+        return jsonify({'statusCode': 200,
                 'message': 'Login Success'}), 200
-        else:
-            app.logger.info('Login Failed')
-            return jsonify({'statusCode': 400, 'errorMessage': 'Bad Login'}), 400
-    return jsonify({'statusCode': 400, 'errorMessage': 'Bad Login'}), 400
+    else:
+        app.logger.info('Login Failed')
+        return jsonify({'statusCode': 400, 'errorMessage': 'Bad Login'}), 400
+
 
 
 # Register
 @app.route('/api/register', methods=['POST'])
 def api_register():
-    users = db_con.users
     content = request.json
     username = content['username']
     password = content['password']
-    existing_user = users.find_one({'username': username})
-    if existing_user is None:
-        hashed = bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt()) ## Hashed pw
-        app.logger.info(hashed)
-        users.insert(
-            {'username': username, 'password': hashed})
+    app.logger.info('Trying Registering')
+    if controller.checkIfRegisterSuccess(username,password):
+        app.logger.info('Register Success')
         return jsonify({'statusCode': 200, 'message': 'User created successed'}), 200
     else:
-        app.logger.info('Register Failed')
+        app.logger.info('Register failed')
         return jsonify({'statusCode': 400, 'errorMessage': 'User already registered'}), 400
-
+        
 
 # Logout
 @app.route('/api/logout', methods=['GET'])
@@ -83,20 +62,27 @@ def api_edit_story():
     return {}
 
 
-# Get all stories
-@app.route('/api/item/all', methods=['GET'])
-def api_all():
-    col = db_con[collection_name_items]
-    cursor = col.find({})
-    return dumps(cursor)
+# # Get all stories
+# @app.route('/api/item/all', methods=['GET'])
+# def api_all():
+#     col = db_con[collection_name_items]
+#     cursor = col.find({})
+#     return dumps(cursor)
 
 
-# Get item by id
-@app.route('/api/item/<int:post_id>', methods=['GET'])
-def api_get_item_by_id(post_id):
-    col = db_con[collection_name_items]
-    cursor = col.find({"id": post_id})
-    return dumps(cursor)
+# # Get item by id
+# @app.route('/api/item/<int:post_id>', methods=['GET'])
+# def api_get_item_by_id(post_id):
+#     col = db_con[collection_name_items]
+#     cursor = col.find({"id": post_id})
+#     return dumps(cursor)
+
+# Delete item by id
+# @app.route('/api/item/<int:post_id>', methods=['GET'])
+# def api_get_item_by_id(post_id):
+#     col = db_con[collection_name_items]
+#     cursor = col.find({"id": post_id})
+#     return dumps(cursor)
 
 
 @app.route('/api/user/<int:user_id>', methods=['GET'])
@@ -110,11 +96,13 @@ def page_not_found(e):
     return "<h1>404</h1><p>The resource could not be found.</p>", 404
 
 
+
+
 # Run the app on 0.0.0.0:5000
 if __name__ == '__main__':
-    prepare_db()
     app.config.update(
         DEBUG=True,
         CSRF_ENABLED=True,
     )
+    controller.prepare()
     app.run(debug=True, host='0.0.0.0')
---
+++ app/controller.py
@@ -0,0 +1,44 @@
+import bcrypt
+import database
+
+# Global variables
+db_con = database.get_db_conn()
+
+def prepare():
+	database.prepare_db()
+	print("ASDASD")
+
+def checkIfloginSuccess(username,password):
+    users = db_con.users
+    login_user = users.find_one({'username': username})
+    print('Trying loggin',username)
+    if login_user:
+        stored_password = login_user['password']
+        ## Comparing stored password and the users hashed password
+        if bcrypt.hashpw(password.encode('utf8'), stored_password) == stored_password: 
+            print('Login Success')
+            return True
+        else:
+            print('Login Failed bad password')
+            return False
+    print('Login Failed user dont exist')
+    return False
+
+def checkIfRegisterSuccess(username,password):
+    try:
+        print("asda")
+        print('Trying registering',username)
+        users = db_con.users
+        existing_user = users.find_one({'username': username})
+        print('Trying registering',username)
+        if existing_user is None:
+            hashed = bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt()) ## Hashed pw
+            print(hashed)
+            users.insert(
+                {'username': username, 'password': hashed})
+            return True
+        else:
+            print('Register Failed')
+            return False
+    except Exception as e:
+        print(str(e))
