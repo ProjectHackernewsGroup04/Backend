258d513306375fa7ff8caeec08f76d3e9fdeab3c
Mikkel Ziemer <mzhjensen@gmail.com>
2018-10-24 16:46:12 +0200

    Now reading auth headers

--- app/app.py
+++ app/app.py
@@ -152,6 +152,8 @@ def status():
 @app.route('/webhook', methods=['POST'])
 def webhook():
     post = request.json
+    print(request.authorization.username, file=sys.stderr)
+    print(request.authorization.password, file=sys.stderr)
     print(post, file=sys.stderr)
     return jsonify({"status": "success"}), 200
     # return jsonify(controller.insert_post(post)), 200
